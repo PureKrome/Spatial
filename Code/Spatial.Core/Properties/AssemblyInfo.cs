b6d307c105a9d9c14d5c91a813960979927e4b66
Pure Krome <pure.krome@world-domination.com.au>
2014-08-05 17:38:11 +1000

    Updated version and NuGet package to 0.2.

--- Code/Spatial.Core/Properties/AssemblyInfo.cs
+++ Code/Spatial.Core/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Spatial.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Spatial.Core : Spatial library (Core components)")]
+[assembly: AssemblyDescription("A few core spatial classes and interfaces.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("World-Domination Pty. Ltd.")]
 [assembly: AssemblyProduct("Spatial.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
--- Code/Spatial.Services/Properties/AssemblyInfo.cs
+++ Code/Spatial.Services/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Spatial.Services")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Spatial.Services : Spatial library")]
+[assembly: AssemblyDescription("Google and Nominatum spatial services.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Spatial.Services")]
+[assembly: AssemblyCompany("World-Domination Pty. Ltd.")]
+[assembly: AssemblyProduct("Spatial.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
