2505b2bfc9535f017049ac4ba58b683bc1b5f130
Pure Krome <pure.krome@world-domination.com.au>
2014-08-05 16:54:30 +1000

    Added component filtering to Google geocoding.

--- Code/Spatial.Services/ApiServices/GoogleMaps/GoogleMapsApiService.cs
+++ Code/Spatial.Services/ApiServices/GoogleMaps/GoogleMapsApiService.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
 using RestSharp;
 using Shouldly;
 using Spatial.Core.Services;
@@ -13,6 +17,11 @@ namespace Spatial.Services.ApiServices.GoogleMaps
 
         public object Geocode(string query)
         {
+            return Geocode(query, null);
+        }
+
+        public object Geocode(string query, ComponentFilters filters)
+        {
             string.IsNullOrWhiteSpace(query).ShouldBe(false);
 
             var client = new RestClient(BaseUrl);
@@ -21,6 +30,15 @@ namespace Spatial.Services.ApiServices.GoogleMaps
             request.AddParameter("address", query);
             request.AddParameter("sensor", "false");
 
+            if (filters != null)
+            {
+                var components = ConvertCompenentFiltersToQuerystringParameter(filters);
+                if (!string.IsNullOrWhiteSpace(components))
+                {
+                    request.AddParameter("components", components);
+                }
+            }
+
             IRestResponse<GoogleMapsResponse> response = client.Execute<GoogleMapsResponse>(request);
 
             return response.Data != null &&
@@ -28,6 +46,57 @@ namespace Spatial.Services.ApiServices.GoogleMaps
                    response.Data.Results.Any()
                 ? response.Data
                 : null;
+        }
+
+        // REF: https://developers.google.com/maps/documentation/geocoding/#ComponentFiltering
+        private static string ConvertCompenentFiltersToQuerystringParameter(ComponentFilters filters)
+        {
+            filters.ShouldNotBe(null);
+
+            var items = new Dictionary<string, string>();
+
+            if (!string.IsNullOrWhiteSpace(filters.Route))
+            {
+                items.Add("route", filters.Route);
+            }
+
+            if (!string.IsNullOrWhiteSpace(filters.Locality))
+            {
+                items.Add("locality", filters.Locality);
+            }
+
+            if (!string.IsNullOrWhiteSpace(filters.AdministrativeArea))
+            {
+                items.Add("administrative_area", filters.AdministrativeArea);
+            }
+
+            if (!string.IsNullOrWhiteSpace(filters.PostalCode))
+            {
+                items.Add("postal_code", filters.PostalCode);
+            }
+
+            if (!string.IsNullOrWhiteSpace(filters.CountryCodeIso))
+            {
+                items.Add("country", filters.CountryCodeIso);
+            }
+
+            if (!items.Any())
+            {
+                return string.Empty;
+            }
+
+            var queryString = new StringBuilder();
+            foreach (var item in items)
+            {
+                if (queryString.Length > 0)
+                {
+                    queryString.Append("|");
+                }
+
+                queryString.AppendFormat("{0}:{1}", item.Key, item.Value);
+            }
+
+            return queryString.ToString();
         }
     }
 }
