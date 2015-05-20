5796c8036b01a395f5c17acf3f6d702f363eb919
Pure Krome <github@world-domination.com.au>
2015-05-20 15:03:04 +1000

    :lipstick:

--- Code/Spatial/ApiServices/GoogleMaps/GoogleMapsApiService.cs
+++ Code/Spatial/ApiServices/GoogleMaps/GoogleMapsApiService.cs
@@ -5,7 +5,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using WorldDomination.Net.Http;
 
 namespace WorldDomination.Spatial.ApiServices.GoogleMaps
 {
