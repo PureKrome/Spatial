a35532c00529127ea059244c0a32683b53e4a844
Pure Krome <github@world-domination.com.au>
2015-05-20 14:57:45 +1000

    :lipstick:

--- Code/Spatial/ApiServices/Nominatum/NominatimApiService.cs
+++ Code/Spatial/ApiServices/Nominatum/NominatimApiService.cs
@@ -4,7 +4,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using WorldDomination.Net.Http;
 
 namespace WorldDomination.Spatial.ApiServices.Nominatum
 {
